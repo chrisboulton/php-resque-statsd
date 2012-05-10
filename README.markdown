4846a37842126e5d32cd7ed4fa1e93ba495b47f5
Chris Boulton <chris.boulton@interspire.com>
2012-05-10 22:48:27 +1000

    actually put this puppy under version control

---
+++ README.markdown
@@ -0,0 +1,121 @@
+php-resque-statsd: PHP Resque StatsD
+==========================================
+
+php-resque-statsd implements [StatsD](http://github.com/etsy/statsd) metric
+tracking into php-resque.
+
+For each job picked up by php-resque, numerous metrics will be submitted to
+StatsD, including counters to track the number of jobs executed, and timers to
+track how much time php-resque workers spend working.
+
+php-resque-statsd also includes support for tracking metrics for jobs scheduled
+with [php-resque-scheduler](http://github.com/chrisboulton/php-resque-scheduler).
+The appropriate listeners to track scheduled jobs are automatically registered,
+so no extra work is required on your behalf.
+
+## Using Loading php-resque-statsd
+
+php-resque-statsd exists as a single class (`lib/ResqueStatsd.php`), which has
+no additional dependencies beyond php-resque itself.
+
+To start tracking your jobs with StatsD, all you need to do is include
+`ResqueStatsd.php` in your project.
+
+If you're starting php-resque with the resque.php script supplied with
+php-resque, all that is
+required is a modification to the bootstrap file you supply to php-resque via
+the `APP_INCLUDE` environment variable:
+
+	require_once '/path/to/ResqueStatsd.php';
+
+### StatsD Connection Details
+
+php-resque-scheduler will automatically check for the following environment
+variables if they exist and use them when connecting to StatsD:
+
+ * `STATSD_HOST` / `STATSD_PORT`
+ * `GRAPHITE_HOST`
+
+To ease integration with existing setups, if either `STATSD_HOST` or
+`GRAPHITE_HOST` include a single colon and then one or more numbers, this will
+be interpretted as a HOST:PORT combination and both the host and port will be
+set accordingly.
+
+If you don't use environment variables in your project, you can still tell
+php-resque-statsd where StatsD is located:
+
+	$host = '127.0.0.1';
+	$port = 8579;
+
+	require_once '/path/to/ResqueStatsd.php';
+	Resque_Statsd::setServer($host, $port);
+
+
+## Metrics
+
+php-resque-statsd prefixes all metrics it generates with `resque`. You can
+override this behavior if desired:
+
+	require_once '/path/to/ResqueStatsd.php';
+	Resque_Statsd::setPrefix('resque.production');
+
+### Queue Based Metrics
+
+The metrics below are tracked for each queue, instead of each unique job:
+
+*   **stats.resque.queue.QUEUE_NAME.enqueued**
+    Counter of the number of jobs enqueued in this queue
+
+*   **stats.resque.queue.QUEUE_NAME.finished**
+	 Counter of the number of jobs successfully processed in this queue
+	
+*   **stats.resque.queue.QUEUE_NAME.failed**
+	 Counter of the number of jobs that failed in this queue
+
+*   **stats.timers.queue.QUEUE_NAME.processed**
+	 Timer for jobs processed in this queue
+
+*   **stats.resque.queue.QUEUE_NAME.scheduled**
+	 If using php-resque-scheduler, number of jobs scheduled for future execution
+	 in this queue
+
+For example, for all jobs executed in the queue `transcode`, the following StatsD
+metrics will be created:
+
+* stats.resque.queue.transcode.enqueued
+* stats.resque.queue.transcode.finished
+* stats.resque.queue.transcode.failed
+* stats.timers.resque.queue.transcode.processed
+* stats.resque.queue.transcode.scheduled
+
+### Job Based Metrics
+
+Metrics are also tracked on a job level:
+
+*   **stats.resque.job.JOB_CLASS.enqueued**
+    Counter for the number of times this job has been enqueued
+
+*   **stats.resque.job.JOB_CLASS.finished**
+    Counter for the number of times this job has been successfully processed
+
+*   **stats.resque.job.JOB_CLASS.failed**
+    Counter for the number of times this job has failed
+
+*   **stats.timers.job.JOB_CLASS.processed**
+    Timer for the amount of time spent processing this job
+
+*   **stats.resque.job.JOB_CLASS.scheduled**
+    If using php-resque-scheduler, number of of times this job has been scheduled
+    for future execution
+
+For example, a job named `Job_SendEmail` the following metrics will be created:
+
+* stats.resque.job.Job_SendEmail.enqueued
+* stats.resque.job.Job_SendEmail.finished
+* stats.resque.job.Job_SendEmail.failed
+* stats.timers.resque.job.Job_SendEmail.processed
+* stats.resque.job.Job_SendEmail.scheduled
+
+## Contributors ##
+
+* chrisboulton
---
+++ lib/ResqueStatsD.php
@@ -0,0 +1,209 @@
+<?php
+/**
+ * php-resque-statsd
+ *
+ * @package		php-resque-statsd
+ * @author		Chris Boulton <chris@bigcommerce.com>
+ * @copyright	(c) 2012 Chris Boulton
+ * @license		http://www.opensource.org/licenses/mit-license.php
+ */
+class ResqueStatsd
+{
+    const STATSD_TIMER   = 'ms';
+    const STATSD_COUNTER = 'c';
+    
+    /**
+     * @var string Prefix to add to metrics submitted to StatsD.
+     */
+    private static $prefix = 'resque';
+    
+    /**
+     * @var string Hostname when connecting to StatsD.
+     */
+    private static $host   = 'localhost';
+    
+    /**
+     * @var int Port StatsD is running on.
+     */
+    private static $port   = 8125;
+
+    /**
+     * Register php-resque-statsd in php-resque.
+     *
+     * Register all callbacks in php-resque for when a job is run. This is
+     * automatically called at the bottom of this script if the appropriate
+     * Resque classes are loaded.
+     */
+    public static function register()
+    {
+        // Core php-resque events
+        Resque_Event::listen('afterEnqueue', 'ResqueStatsd::afterEnqueue');
+        Resque_Event::listen('beforeFork', 'ResqueStatsd::beforeFork');
+        Resque_Event::listen('afterPerform', 'ResqueStatsd::afterPerform');
+        Resque_Event::listen('onFailure', 'ResqueStatsd::onFailure');
+        
+        // Add support for php-resque-scheduler
+        Resque_Event::listen('afterSchedule', 'ResqueStatsd::afterSchedule');
+    }
+    
+    /**
+     * Set the host/port combination of StatsD.
+     *
+     * @param string $host Hostname/IP of StatsD server.
+     * @param int $port Port StatsD is listening on.
+     */
+    public static function setHost($host, $port)
+    {
+        self::$host = $host;
+        self::$port = $port;
+    }
+    
+    /**
+     * Override the prefix for metrics that are submitted to StatsD.
+     *
+     * @param string $prefix Prefix to use for metrics.
+     */
+    public static function setPrefix($prefix)
+    {
+        self::$prefix = $prefix;
+    }
+    
+    /**
+     * Submit metrics for a queue and job whenever a job is pushed to a queue.
+     *
+     * @param string $class Class name of the job that was just created.
+     * @param array $args Arguments passed to the job.
+     * @param string $queue Name of the queue the job was created in.
+     */
+    public static function afterEnqueue($class, $args, $queue)
+    {
+        self::sendMetric(self::STATSD_COUNTER, 'queue.' . $queue . '.enqueued', 1);
+        self::sendMetric(self::STATSD_COUNTER, 'job.' . $class . '.enqueued', 1);
+    }
+    
+    /**
+     * Submit metrics for a queue and job whenever a job is scheduled in php-resque-scheduler.
+     *
+     * @param DateTime|int $at Instance of PHP DateTime object or int of UNIX timestamp.
+     * @param string $class Class name of the job that was just created.
+     * @param array $args Arguments passed to the job.
+     * @param string $queue Name of the queue the job was created in.
+     */
+    public static function afterSchedule($at, $queue, $class, $args)
+    {
+        self::sendMetric(self::STATSD_COUNTER, 'queue.' . $queue . '.scheduled', 1);
+        self::sendMetric(self::STATSD_COUNTER, 'job.' . $class . '.scheduled', 1);
+    }
+    
+    /**
+     * Begin tracking execution time before forking out to run a job in a php-resque worker.
+     *
+     * Time tracking begins in `beforeFork` to ensure that the time spent for forking
+     * and any hooks registered for `beforePerform` is also tracked.
+     *
+     * @param Resque_Job $job Instance of Resque_Job for the job about to be run.
+     */
+    public static function beforeFork(Resque_Job $job)
+    {
+        $job->statsDStartTime = microtime(true);
+    }
+    
+    /**
+     * Submit metrics for a queue and job as soon as job has finished executing successfully.
+     *
+     * @param Resque_Job $job Instance of Resque_Job for the job that's just been executed.
+     */
+    public static function afterPerform(Resque_Job $job)
+    {
+        self::sendMetric(self::STATSD_COUNTER, 'queue.' . $job->queue . '.finished', 1);
+        self::sendMetric(self::STATSD_COUNTER, 'job.' . $job->payload['class'] . '.finished', 1);
+        
+        $executionTime = round(microtime(true) - $job->statsDStartTime) * 1000;
+        self::sendMetric(self::STATSD_TIMER, 'queue.' . $job->queue . '.processed', $executionTime);
+        self::sendMetric(self::STATSD_TIMER, 'job.' . $job->payload['class'] . '.processed', $executionTime);
+    }
+
+    /**
+     * Submit metrics for a queue and job whenever a job fails to run.
+     *
+     * @param Exception $e Exception thrown by the job.
+     * @param Resque_Job $job Instance of Resque_Job for the job that failed.
+     */
+    public static function onFailure(Exception $e, Resque_Job $job)
+    {
+        self::sendMetric(self::STATSD_COUNTER, 'queue.' . $job->queue . '.failed', 1);
+        self::sendMetric(self::STATSD_COUNTER, 'job.' . $job->payload['class'] . '.failed', 1);
+    }
+    
+    /**
+     * Return a tuple containing the StatsD host and port to submit metrics to.
+     *
+     * Looks for environment variables STATSD_HOST, then GRAPHITE_HOST before
+     * resorting to the host/port combination passed to `register`, or defaulting
+     * to localhost. Port is determined in much the same way, however looks for
+     * the STATSD_PORT environment variable.
+     *
+     * If the host variable includes a single colon, the first part of the string
+     * is used for the host, and the second part for the port.
+     *
+     * @return array Array containing host and port.
+     */
+    private static function getStatsDHost()
+    {
+        $host = self::$host;
+        $port = self::$port;
+
+        if (!empty($_ENV['STATSD_HOST'])) {
+            $host = $_ENV['STATSD_HOST'];
+        }
+        else if(!empty($_ENV['GRAPHITE_HOST'])) {
+            $host = $_ENV['GRAPHITE_HOST'];
+        }
+        
+        if (!empty($_ENV['STATSD_PORT'])) {
+            $port = $_ENV['STATSD_PORT'];
+        }
+
+        if (substr_count($host, ':') == 1) {
+            list($host, $port) = explode(':', $host);
+        }
+    
+        return array($host, $port);
+    }
+    
+    /**
+     * Submit a metric of the given type, name and value to StatsD.
+     *
+     * @param string $type Type of metric to submit (c for counter, ms for timer)
+     * @param string $name Name of the metric to submit. Will be prefixed.
+     * @param int $value Value of the metric to submit.
+     *
+     * @return boolean True if the metric was submitted successfully.
+     */
+    private static function sendMetric($type, $name, $value)
+    {
+        list($host, $port) = self::getStatsDHost();
+        
+        if (empty($host) || empty($port)) {
+            return false;
+        }
+
+        $fp = fsockopen('udp://' . $host, $port, $errno, $errstr);
+        if (!$fp || $errno > 0) {
+            return false;
+        }
+        
+        $metric = self::$prefix . '.' . $name . ':' . $value . '|' . $type;
+        if (!fwrite($fp, $metric)) {
+            return false;
+        }
+
+        fclose($fp);
+        return true;
+    }
+}
+
+// Automatically register if Resque is available
+if (class_exists('Resque') && !defined('RESQUESTATSD_DONT_REGISTER')) {
+    ResqueStatsD::register();
+}
